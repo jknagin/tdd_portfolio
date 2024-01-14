70fa78ded05b20872ead69c0715e7e1d38561a6a
Jay Nagin <jknagin@gmail.com>
2024-01-14 14:47:15 -0800

    Created module

--- src/lib.rs
+++ src/lib.rs
@@ -1,137 +1,141 @@
-use chrono::NaiveDateTime;
-use std::collections::HashMap;
+mod portfolio_mod {
+    use chrono::NaiveDateTime;
+    use std::collections::HashMap;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-enum TransactionType {
-    Purchase,
-    Sell,
-}
-
-#[allow(dead_code)]
-#[derive(Debug, PartialEq, Eq)]
-struct PurchaseRecord {
-    date: NaiveDateTime,
-    shares: u32,
-    transaction_type: TransactionType,
-}
-
-struct Portfolio {
-    holdings: HashMap<String, u32>,
-    purchase_records: HashMap<String, Vec<PurchaseRecord>>,
-}
-
-#[derive(Debug, thiserror::Error)]
-enum PortfolioError {
-    #[error("Cannot perform transaction with zero shares")]
-    ZeroShares,
-
-    #[error("Cannot sell more shares than owned")]
-    InvalidSell,
-
-    #[error("No history for symbol")]
-    NoSymbolHistory,
-
-    #[error("Too many shares puchased")]
-    InvalidPurchase,
-}
-
-type PortfolioResult<T> = Result<T, PortfolioError>;
-
-#[allow(dead_code)]
-impl Portfolio {
-    const FIXED_EPOCH_TIME_MS: i64 = 0;
-
-    const EMPTY_PURCHASE_RECORD: Vec<PurchaseRecord> = vec![];
-
-    fn fixed_date_time() -> NaiveDateTime {
-        NaiveDateTime::from_timestamp_millis(Self::FIXED_EPOCH_TIME_MS).unwrap()
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub enum TransactionType {
+        Purchase,
+        Sell,
     }
 
-    fn new() -> Self {
-        Self {
-            holdings: HashMap::new(),
-            purchase_records: HashMap::new(),
+    #[allow(dead_code)]
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct PurchaseRecord {
+        pub date: NaiveDateTime,
+        pub shares: u32,
+        pub transaction_type: TransactionType,
+    }
+
+    pub struct Portfolio {
+        holdings: HashMap<String, u32>,
+        purchase_records: HashMap<String, Vec<PurchaseRecord>>,
+    }
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum PortfolioError {
+        #[error("Cannot perform transaction with zero shares")]
+        ZeroShares,
+
+        #[error("Cannot sell more shares than owned")]
+        InvalidSell,
+
+        #[error("No history for symbol")]
+        NoSymbolHistory,
+
+        #[error("Too many shares puchased")]
+        InvalidPurchase,
+    }
+
+    pub type PortfolioResult<T> = Result<T, PortfolioError>;
+
+    #[allow(dead_code)]
+    impl Portfolio {
+        const FIXED_EPOCH_TIME_MS: i64 = 0;
+
+        const EMPTY_PURCHASE_RECORD: Vec<PurchaseRecord> = vec![];
+
+        pub fn fixed_date_time() -> NaiveDateTime {
+            NaiveDateTime::from_timestamp_millis(Self::FIXED_EPOCH_TIME_MS).unwrap()
         }
-    }
 
-    fn is_empty(&self) -> bool {
-        self.holdings.is_empty()
-    }
-
-    fn validate_share_count(shares: u32) -> PortfolioResult<()> {
-        if shares == 0 {
-            return Err(PortfolioError::ZeroShares);
+        pub fn new() -> Self {
+            Self {
+                holdings: HashMap::new(),
+                purchase_records: HashMap::new(),
+            }
         }
-        Ok(())
-    }
 
-    fn purchase(&mut self, symbol: &str, shares: u32) -> PortfolioResult<()> {
-        self.transact(symbol, shares, TransactionType::Purchase)
-    }
+        pub fn is_empty(&self) -> bool {
+            self.holdings.is_empty()
+        }
 
-    fn sell(&mut self, symbol: &str, shares: u32) -> PortfolioResult<()> {
-        self.transact(symbol, shares, TransactionType::Sell)
-    }
+        fn validate_share_count(shares: u32) -> PortfolioResult<()> {
+            if shares == 0 {
+                return Err(PortfolioError::ZeroShares);
+            }
+            Ok(())
+        }
 
-    fn transact(
-        &mut self,
-        symbol: &str,
-        shares: u32,
-        transaction_type: TransactionType,
-    ) -> PortfolioResult<()> {
-        Self::validate_share_count(shares)?;
-        self.update_holdings(symbol, shares, transaction_type.clone())?;
-        self.update_purchase_records(symbol, shares, transaction_type.clone())
-    }
+        pub fn purchase(&mut self, symbol: &str, shares: u32) -> PortfolioResult<()> {
+            self.transact(symbol, shares, TransactionType::Purchase)
+        }
 
-    fn update_holdings(
-        &mut self,
-        symbol: &str,
-        shares: u32,
-        transaction_type: TransactionType,
-    ) -> PortfolioResult<()> {
-        let count = self.holdings.entry(symbol.to_string()).or_default();
-        let new_shares = match transaction_type {
-            TransactionType::Purchase => count
-                .checked_add(shares)
-                .ok_or(PortfolioError::InvalidPurchase),
+        pub fn sell(&mut self, symbol: &str, shares: u32) -> PortfolioResult<()> {
+            self.transact(symbol, shares, TransactionType::Sell)
+        }
 
-            TransactionType::Sell => count.checked_sub(shares).ok_or(PortfolioError::InvalidSell),
-        }?;
-        *count = new_shares;
-        Ok(())
-    }
+        fn transact(
+            &mut self,
+            symbol: &str,
+            shares: u32,
+            transaction_type: TransactionType,
+        ) -> PortfolioResult<()> {
+            Self::validate_share_count(shares)?;
+            self.update_holdings(symbol, shares, transaction_type.clone())?;
+            self.update_purchase_records(symbol, shares, transaction_type.clone())
+        }
 
-    fn update_purchase_records(
-        &mut self,
-        symbol: &str,
-        shares: u32,
-        transaction_type: TransactionType,
-    ) -> PortfolioResult<()> {
-        let records = self.purchase_records.entry(symbol.to_string()).or_default();
-        records.push(PurchaseRecord {
-            date: Self::fixed_date_time(),
-            shares,
-            transaction_type,
-        });
-        Ok(())
-    }
+        fn update_holdings(
+            &mut self,
+            symbol: &str,
+            shares: u32,
+            transaction_type: TransactionType,
+        ) -> PortfolioResult<()> {
+            let count = self.holdings.entry(symbol.to_string()).or_default();
+            let new_shares = match transaction_type {
+                TransactionType::Purchase => count
+                    .checked_add(shares)
+                    .ok_or(PortfolioError::InvalidPurchase),
 
-    fn share_count(&self, symbol: &str) -> u32 {
-        *self.holdings.get(symbol).unwrap_or(&0)
-    }
+                TransactionType::Sell => {
+                    count.checked_sub(shares).ok_or(PortfolioError::InvalidSell)
+                }
+            }?;
+            *count = new_shares;
+            Ok(())
+        }
 
-    fn get_purchase_record(&self, symbol: &str) -> PortfolioResult<&[PurchaseRecord]> {
-        self.purchase_records
-            .get(symbol)
-            .map(|x| x.as_slice())
-            .ok_or(PortfolioError::NoSymbolHistory)
+        fn update_purchase_records(
+            &mut self,
+            symbol: &str,
+            shares: u32,
+            transaction_type: TransactionType,
+        ) -> PortfolioResult<()> {
+            let records = self.purchase_records.entry(symbol.to_string()).or_default();
+            records.push(PurchaseRecord {
+                date: Self::fixed_date_time(),
+                shares,
+                transaction_type,
+            });
+            Ok(())
+        }
+
+        pub fn get_share_count(&self, symbol: &str) -> u32 {
+            *self.holdings.get(symbol).unwrap_or(&0)
+        }
+
+        pub fn get_purchase_record(&self, symbol: &str) -> PortfolioResult<&[PurchaseRecord]> {
+            self.purchase_records
+                .get(symbol)
+                .map(|x| x.as_slice())
+                .ok_or(PortfolioError::NoSymbolHistory)
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::portfolio_mod::*;
     use rstest::*;
 
     const IBM: &str = "IBM";
@@ -163,12 +167,12 @@ mod tests {
 
     #[rstest]
     fn answers_zero_for_share_count_of_unpurchased_symbol(portfolio: Portfolio) {
-        assert_eq!(portfolio.share_count(UNPURCHASED_SYMBOL), 0);
+        assert_eq!(portfolio.get_share_count(UNPURCHASED_SYMBOL), 0);
     }
 
     #[rstest]
     fn answers_nonzero_for_share_count_of_purchased_symbol(portfolio_with_ibm: Portfolio) {
-        assert!(portfolio_with_ibm.share_count(IBM) > 0);
+        assert!(portfolio_with_ibm.get_share_count(IBM) > 0);
     }
 
     #[rstest]
@@ -185,7 +189,7 @@ mod tests {
     ) -> PortfolioResult<()> {
         let aapl_shares = 3;
         portfolio_with_ibm.purchase(AAPL, aapl_shares)?;
-        assert_eq!(portfolio_with_ibm.share_count(AAPL), aapl_shares);
+        assert_eq!(portfolio_with_ibm.get_share_count(AAPL), aapl_shares);
         Ok(())
     }
 
@@ -195,7 +199,7 @@ mod tests {
     ) -> PortfolioResult<()> {
         portfolio.purchase(IBM, 1)?;
         portfolio.purchase(IBM, 2)?;
-        assert_eq!(portfolio.share_count(IBM), 3);
+        assert_eq!(portfolio.get_share_count(IBM), 3);
         Ok(())
     }
 
@@ -203,7 +207,7 @@ mod tests {
     fn reduce_share_count_of_symbol_on_sell(mut portfolio: Portfolio) -> PortfolioResult<()> {
         portfolio.purchase(IBM, 5)?;
         portfolio.sell(IBM, 3)?;
-        assert_eq!(portfolio.share_count(IBM), 2);
+        assert_eq!(portfolio.get_share_count(IBM), 2);
         Ok(())
     }
 
